7bcc7cda59c2b67dfef193480adaeb1889e8b35b
Ranieli <ranielisilveira@hotmail.com>
2022-08-17 18:08:36 -0300

    Create user route, controller

--- routes/AppTakeCare/users.php
+++ routes/AppTakeCare/users.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\Users\UsersController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => '\app\Http\Controllers'], function () {
-    Route::apiResource('users', UsersController::class)->only(['index', 'show', 'store', 'update', 'destroy']);
-});
+
+Route::get('users', 'UsersController@index')->name('users.index');
